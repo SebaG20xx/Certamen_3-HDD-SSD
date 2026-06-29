4ce4efe1a4adfca85024d1aeed9eea1610384dcd
Sebastián Guevara <82485346+SebaG20xx@users.noreply.github.com>
2026-06-29 19:00:00 -0400

    feat: inicializar proyecto Spring Boot

---
+++ src/test/java/cl/usm/sansaweigh/SansaweighApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.sansaweigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansaweighApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
