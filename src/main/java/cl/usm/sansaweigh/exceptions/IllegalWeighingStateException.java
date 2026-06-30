599fd92df11b2725fbb5bc3ba48921de23cfae4d
Sebastián Guevara <82485346+SebaG20xx@users.noreply.github.com>
2026-06-29 21:38:51 -0400

    se añada la excepción IllegalWeighingStateException

---
+++ src/main/java/cl/usm/sansaweigh/exceptions/IllegalWeighingStateException.java
@@ -0,0 +1,8 @@
+package cl.usm.sansaweigh.exceptions;
+
+public class IllegalWeighingStateException extends RuntimeException {
+
+    public IllegalWeighingStateException(String message) {
+        super(message);
+    }
+}
